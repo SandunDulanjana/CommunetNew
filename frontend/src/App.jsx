fe9503e1723642ebec46e904701efa6e25df750c
KavishkaMalmi <kavishkamalmi25@gmail.com>
2025-03-25 11:44:35 +0530

    add finance landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,6 +27,7 @@ import Checkout from './pages/CheckOut';
 import Payment from "./pages/Payment";
 import Success from "./pages/Success";
 import Expences from './pages/Expences'
+import FinanceHome from './pages/FinanceHome';
 
 
 function App() {
@@ -57,6 +58,8 @@ function App() {
         <Route path="/payment" element={<Payment />} />
         <Route path="/success" element={<Success />} />
         <Route path='/expences' element={<Expences/>} />
+        <Route path='/financeHome' element={<FinanceHome/>} />
+
         <Route path='/AddEvent' element = {<AddEvent/>}></Route>
       
       </Routes>
